35f811c696d28360ad9ea3bc75e867ab247856cb
abdulaki91 <abdulakimustefa@gmail.com>
2025-11-27 17:38:14 +0300

    REMOVE THESE COMNT

--- Frontend/src/pages/SuperAdminDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export const SuperAdminDashboard = () => {
-  return (
-    <div>
-      <h1>Super Admin Dashboard</h1>
-      <p>Welcome, Super Admin! Manage everything here.</p>
-    </div>
-  );
-};

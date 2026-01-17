83572d7240fbd6b0f5ebffa73c8a95e7b8957e5b
abdulaki <abdulakimustefa@gmail.com>
2026-01-17 13:15:58 +0300

    fix: add missing whitespace in ProjectFilters component

--- Frontend/src/components/ProjectFilters.jsx
+++ Frontend/src/components/ProjectFilters.jsx
@@ -23,7 +23,7 @@ export default function ProjectFilters({
   courses = [],
 }) {
   return (
-    <div className="flex flex-col md:flex-row gap-4">
+    <div className="flex flex-col md:flex-row gap-4 ">
       <div className="flex-1 relative">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-slate-400 dark:text-slate-500" />
         <Input
